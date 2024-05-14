c6118bfad2c8f1a28deec825455e97eda7bbd75a
Alex Gaspar Linux <alex.gaspar@capere.inf.br>
2024-05-14 09:32:56 -0300

    fix: utf8_encode deprecated for mb_convert_encoding

--- lib/OfxParser/Parser.php
+++ lib/OfxParser/Parser.php
@@ -50,7 +50,9 @@ class Parser
     public function loadFromString($ofxContent)
     {
         $ofxContent = str_replace(["\r\n", "\r"], "\n", $ofxContent);
-        $ofxContent = utf8_encode($ofxContent);
+        //$ofxContent = utf8_encode($ofxContent);
+        $ofxContent = mb_convert_encoding($ofxContent, "UTF-8", mb_detect_encoding($ofxContent));
+
 
         $sgmlStart = stripos($ofxContent, '<OFX>');
         $ofxHeader =  trim(substr($ofxContent, 0, $sgmlStart));
